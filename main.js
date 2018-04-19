b1273fff617a5ab7b6b104a6f487fd5d02a21906
Magisteri <38146215+Magisteri@users.noreply.github.com>
2018-04-19 03:25:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,26 +3,19 @@ const ham = document.querySelector('.ham');
 const links = document.querySelector('#links');
 const smNav = document.querySelector('.smNav');
 const header = document.querySelector('#header');
+const light = document.querySelector('.light');
+const root = document.querySelector(':root');
 var i = 0;
+var l = 0;
 
 function sizeCheck() {
     if (window.matchMedia("(max-width: 1080px)").matches) {
         links.style.height = "0";
         links.style.display = "none";
-        smNav.style.borderBottom = "1px solid white";
         i = 0;
     } else {
         links.style.height = "auto";
         links.style.display = "grid";
-        smNav.style.borderBottom = "0";
-    }
-}
-
-function check() {
-    if (window.matchMedia("(max-width: 1080px)").matches) {
-        smNav.style.borderBottom = "1px solid white";
-    } else {
-        header.style.borderBottom = "1px solid white";
     }
 }
 
@@ -40,6 +33,20 @@ function openMenu() {
     }
 }
 
+function switchTheme() {
+    if (l == 0) {
+        document.documentElement.style.setProperty('--bkgd', '#0A0A0A');
+        document.documentElement.style.setProperty('--txt', 'white');
+        document.documentElement.style.setProperty('--acc', '#333333');
+        l += 1;
+    } else {
+        document.documentElement.style.setProperty('--bkgd', '#F7F7F7');
+        document.documentElement.style.setProperty('--txt', '#0A0A0A');
+        document.documentElement.style.setProperty('--acc', 'rgb(180, 180, 180)');
+        l = 0;
+    }
+}
+
+light.addEventListener('click', switchTheme);
 window.addEventListener('resize', sizeCheck);
-window.addEventListener('load', check);
 ham.addEventListener('click', openMenu);
