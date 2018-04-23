85b1b41ad8a371c31782964deff117bcb0e87f18
Magisteri <38146215+Magisteri@users.noreply.github.com>
2018-04-23 02:52:06 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const smNav = document.querySelector('.smNav');
 const header = document.querySelector('#header');
 const light = document.querySelector('.light');
 const root = document.querySelector(':root');
+const btn = document.querySelector('#myBtn');
 var i = 0;
 var l = 0;
 
@@ -12,10 +13,12 @@ function sizeCheck() {
     if (window.matchMedia("(max-width: 1080px)").matches) {
         links.style.height = "0";
         links.style.display = "none";
+        header.style.height = "65px";
         i = 0;
     } else {
         links.style.height = "auto";
         links.style.display = "grid";
+        header.style.height = "auto";
     }
 }
 
@@ -24,10 +27,12 @@ function openMenu() {
         if (i == 0) {
             links.style.height = "auto";
             links.style.display = "block";
+            header.style.height = "auto";
             i += 1;
         } else {
             links.style.height = "0";
             links.style.display = "none";
+            header.style.height = "65px";
             i = 0;
         }
     }
@@ -37,7 +42,7 @@ function switchTheme() {
     if (l == 0) {
         document.documentElement.style.setProperty('--bkgd', '#0A0A0A');
         document.documentElement.style.setProperty('--txt', 'white');
-        document.documentElement.style.setProperty('--acc', '#333333');
+        document.documentElement.style.setProperty('--acc', '#606060');
         l += 1;
     } else {
         document.documentElement.style.setProperty('--bkgd', '#F7F7F7');
@@ -46,6 +51,25 @@ function switchTheme() {
         l = 0;
     }
 }
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.style.opacity = "0.25";
+        btn.style.cursor = "pointer";
+    } else {
+        btn.style.opacity = "0";
+        btn.style.cursor = "default";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
 light.addEventListener('click', switchTheme);
 window.addEventListener('resize', sizeCheck);
